e1d6957b1385a4e4ec262d470c20ed90eeb18a8e
Mario De Pra <mariodepra@on.br>
2018-09-06 20:29:28 -0400

    adding modules for measuring the slope

--- cana/__init__.py
+++ cana/__init__.py
@@ -2,3 +2,4 @@ from cana.util import find_nearest, kwargupdate, Parameter
 from cana.spec import loadspec, Spectrum
 from cana.errormodels import SpecError
 from cana.bandutils import depth, Depth, Continuum
+from cana.slopeutils import slope, Slope
