d85b184d9fc0e682291b62608108e67e8c44a1b0
Mario De Pra <mariodepra@on.br>
2019-11-22 08:27:46 -0500

    testing

--- docs/conf.py
+++ docs/conf.py
@@ -12,7 +12,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('..'))
+# sys.path.insert(0, os.path.abspath('..'))
 import sphinx_bootstrap_theme
 import sphinx_gallery
 from sphinx_gallery.sorting import FileNameSortKey
