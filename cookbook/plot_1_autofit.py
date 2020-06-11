4a5da1af86228a6651200c6abe0a44f633c70783
Sebastian <sebastianlsteele@knights.ucf.edu>
2020-06-11 12:31:16 -0400

    made changes for readability

--- cookbook/plot_1_autofit.py
+++ cookbook/plot_1_autofit.py
@@ -6,18 +6,17 @@ Autofit a Spectra
 import cana
 import matplotlib.pyplot as plt
 
-# First load an spectrum
+# First load an spectrum, we will just gonna use one from the available datasets.
 # you can do: spec = cana.loadspec('path to your spectrum file')
 
-# For the example, we will just gonna use one from the available datasets.
-
+# See spec.py Spectrum class for spec attributes
 spec = cana.datasets.getspectrum('000334', ref='primass')
 
 # fitting the spectrum, you can give the minimal and maximal order that could
-# be fitted. Default is 1 and 12.
-specfit, coefs = spec.autofit(degree_min=1, degree_max=12)
+# be fitted. Default: degree_min=1, degree_max=12.
+specfit, coefs = spec.autofit()
 
-# Ploting
+# Plotting
 plt.plot(spec.w, spec.r, c='0.3', lw=1, label='Spectrum')
 plt.plot(specfit.w, specfit.r, c='r', label='Fit')
 plt.legend()
