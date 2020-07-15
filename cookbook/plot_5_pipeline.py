c64d612f513b70ba0bdd462d79ef882e567d3cda
Sebastian <sebastianlsteele@knights.ucf.edu>
2020-07-15 13:14:26 -0400

    old change, forgot to commit

--- cookbook/plot_5_pipeline.py
+++ cookbook/plot_5_pipeline.py
@@ -17,21 +17,32 @@ spec = cana.datasets.getspectrum('000752', ref='primass')
 # and can simply be setted as params='default'
 
 # The Slope method
-slope = cana.Slope(wmin=0.5, wmax=0.9, norm=0.55)
+# Defaults: wmin=0.4, wmax=0.9, norm=0.55, errormethod='rms',
+#           error_param=None, montecarlo=1000, speckwargs=None
+slope = cana.Slope()
+
 # The Taxonomy method
-tax = cana.Taxonomy(system='bus')
+# Defaults: system='demeo', method='chisquared', return_n=3, norm=0.55,
+#             fitspec=True, speckwargs=None
+tax = cana.Taxonomy()
+
 # The band method
+# Defaults: lowerwindow=0.04, upperwindow=0.04
 continuum = cana.Continuum(lowerwindow=0.03, upperwindow=0.03)
+
+# Depth Defults: wmin=0.54, wmax=0.88, continuum=Continuum()
 band = cana.Depth(wmin=0.55, wmax=0.85, continuum=continuum)
+
 isband = {'min_depth': 1, 'theoric_min': 0.7,
           'max_dist': 0.5, 'sigma': 3}
 
 # Running the pipeline
 # if spec is a list of spectra path, outplot can be setted as
 # a directory to save the plots
-
-pipe = cana.primitive_visible(spec, params=[slope, tax, band],
-                              isband=isband, outplot='show')
+# Defaults: params='default', error=SpecError(),
+#                      isband=None, speckwargs=None, outplot=None,
+#                      verbose=False
+pipe = cana.primitive_visible(spec, params=[slope, tax, band],isband=isband, outplot='show')
 
 # Printing results
 print(pipe)
