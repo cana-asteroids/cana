2523960426b90e2f9c2228ab77570a1a0a5d0d01
depra <mariodepra@ucf.edu>
2021-11-01 18:13:04 -0400

    changing input of NEATM class init

--- cana/thermal/models.py
+++ cana/thermal/models.py
@@ -1,8 +1,10 @@
 r"""Thermal Models."""
 
+from functools import partial
 import numpy as np
 from scipy.integrate import dblquad
 from .. import Spectrum
+
 sig = 5.670373 * (10**-8)          # Stefan-Boltzmann Constant [W/m^2/K^4]
 SUNc = 1367.567          # solar constant at 1 AU in [W/m^2]
 AU = 1.49597870 * (10**8)        # AU in [km]
@@ -11,16 +13,36 @@ c_light = 2.99792458 * (10**8)  # speed of light [m/s]
 h_planck = 6.6260755 * (10**-34)	 # Planck constante Js
 
 
+def planckian(wavelength, temp):
+    r"""Calculate the planck function.
+
+    Parameters
+    ----------
+    wavelength: float
+        The lambda in unit of microns.
+
+    temp: float
+        The temperature in unit of Kelvin.
+
+    Returns
+    -------
+    The black-body spectral radiance
+    """
+    wavelength = wavelength * 1e-6
+    aux = np.exp(h_planck * c_light / k_bol / wavelength / temp)
+    B = 2 * h_planck * c_light / (wavelength**3 * (aux - 1.0))
+    return B
+
+
 class ThermalCore(object):
     r"""
     """
 
-    def __init__(self, albedo, eta=0.9, epsilon=0.9, G=0.15):
+    def __init__(self, eta=0.9, epsilon=0.9, G=0.15):
         r"""
         """
         self.eta = eta
         self.epsilon = epsilon
-        self.albedo = albedo
         self.G = G
 
     def phase_integral(self):
@@ -39,17 +61,17 @@ class ThermalCore(object):
         """
         return 0.29 + 0.684 * self.G
 
-    def bolometric_albedo(self):
+    def bolometric_albedo(self, albedo):
         r"""Bolometric Bond Albedo.
 
         Uses the geometric albedo and the phase integral to calculate The
         bolometric bond albedo.
         """
-        return self.albedo * self.phase_integral()
+        return albedo * self.phase_integral()
 
-    def subsolar_temperature(self, r):
+    def subsolar_temperature(self, r, albedo):
         r"""Calculate the sub-solar point temperature."""
-        A = self.bolometric_albedo()
+        A = self.bolometric_albedo(albedo)
         Tss = (SUNc * (1-A) / ((r**2) * self.epsilon * sig * self.eta))**0.25
         return Tss
 
@@ -60,7 +82,7 @@ class NEATM(ThermalCore):
     Reference: Harris (1998, Icarus, 131, 291-301).
     """
 
-    def __init__(self, r, delta, albedo, diameter, phase_angle=0,
+    def __init__(self, r, delta, phase_angle=0,
                  eta=0.9, epsilon=0.9, G=0.15):
         r"""Initialize the model.
 
@@ -84,52 +106,34 @@ class NEATM(ThermalCore):
         eta: float
             the NEATM eta parameter
         """
-        ThermalCore.__init__(self, albedo, eta=eta,
+        ThermalCore.__init__(self, eta=eta,
                              epsilon=epsilon, G=G)
         self.r = r
-        self.D = diameter
+        # self.D = diameter
         self.delta = delta
         self.phase_angle = phase_angle
         self.alpha = np.radians(phase_angle)
-        self.T0 = self.subsolar_temperature(r)
-        self._integral = np.vectorize(self._integral_aux)
+        # self.T0 = self.subsolar_temperature(r)
+        # self._integral = np.vectorize(self._integral_aux)
 
-    @staticmethod
-    def planckian(wavelength, temp):
-        r"""Calculate the planck function.
 
-        Parameters
-        ----------
-        wavelength: float
-            The lambda in unit of microns.
 
-        temp: float
-            The temperature in unit of Kelvin.
-
-        Returns
-        -------
-        The black-body spectral radiance
-        """
-        wavelength = wavelength * 1e-6
-        aux = np.exp(h_planck * c_light / k_bol / wavelength / temp)
-        B = 2 * h_planck * c_light / (wavelength**3 * (aux - 1.0))
-        return B
-
-    def _integral_aux(self, wavelength):
+    def _integral_aux(self, wavelength, albedo=0.05, diameter=300):
         r"""Auxiliary method to calculate the integral per wavelength."""
         aux = dblquad(self.flux_aux, 0.0, np.pi/2.0,
                       lambda phi: -np.pi/2.0 + phi, lambda phi: np.pi/2.0,
-                      args=(wavelength, self.alpha))[0]
+                      args=(wavelength, self.alpha, albedo, diameter))[0]
         return aux
 
-    def flux_aux(self, phi, theta, wavelength, alpha):
+    def flux_aux(self, phi, theta, wavelength, alpha, albedo, diameter):
         r"""Auxiliary method to calculate the flux."""
-        T = self.T0 * np.cos(phi)**0.25 * np.cos(theta)**0.25
-        B = self.planckian(wavelength, T)
+        T0 = self.subsolar_temperature(self.r, albedo)
+        T = T0 * np.cos(phi)**0.25 * np.cos(theta)**0.25
+        B = planckian(wavelength, T)
         a = B * np.pi * np.cos(phi)**2 * np.cos(theta - self.alpha)
         return a
 
-    def flux(self, wavelengths=[0.5, 2.5, 4.5]):
+    def flux(self, albedo, diameter, wavelengths=[0.5, 2.5, 4.5]):
         r"""Calculate the thermal flux.
 
         Parameters
@@ -142,9 +146,13 @@ class NEATM(ThermalCore):
         ThermalSpec
             The thermal spectrum array
         """
-        flux = self._integral(wavelengths)
+        integral_aux = partial(self._integral_aux,
+                               albedo=albedo,
+                               diameter=diameter)
+        integral = np.vectorize(integral_aux)
+        flux = integral(wavelengths)
         # fixing units
-        norm = (self.epsilon * (self.D / self.delta)**2
+        norm = (self.epsilon * (diameter / self.delta)**2
                 / np.pi / 2.0) * 4.468370499519714 * 1e9
 
         flux *= norm
@@ -152,5 +160,5 @@ class NEATM(ThermalCore):
         return Spectrum(wavelengths, flux, label='thermal-flux')
 
 
-class ThermalSpec(Spectrum):
+class ThermalSpec(Spectrum, ThermalCore):
     r""" Thermal Spectrum."""
